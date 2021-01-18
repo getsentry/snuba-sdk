6a586d02169b20b1cc769cc7d0beb98bb0a4bf67
Evan Hicks <evan.hicks@sentry.io>
2021-01-18 12:39:44 -0500

    fix lint

--- snuba_sdk/expressions.py
+++ snuba_sdk/expressions.py
@@ -5,7 +5,7 @@ from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from datetime import date, datetime
 from enum import Enum
-from typing import List, Optional, Set, Union
+from typing import Any, List, Optional, Sequence, Set, Union
 
 from snuba_sdk.snuba import check_array_type, is_aggregation_function
 
