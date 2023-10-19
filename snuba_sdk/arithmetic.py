65234bba29d5b45ea172b1558d459db4bbaaf941
Enoch Tang <enoch.tang@sentry.io>
2023-10-19 16:12:12 -0400

    fix lint

--- snuba_sdk/arithmetic.py
+++ snuba_sdk/arithmetic.py
@@ -8,6 +8,7 @@ from snuba_sdk.expressions import InvalidExpressionError
 class InvalidArithmeticError(InvalidExpressionError):
     pass
 
+
 class ArithmeticFunction(Enum):
     PLUS = "plus"
     MINUS = "minus"
